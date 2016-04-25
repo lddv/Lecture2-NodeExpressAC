0deaa42037909adab5de8fff10a019293de78bc8
Leonardo Dias de Vasconcelos <leodias.vasconcelos@yahoo.com.br>
2016-04-25 10:35:15 -0300

    Exercise 2: Query users or list them all.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 var express = require('express');
 var app = express();
 
-// Exercise 1:
-// Create a Express server that respond with 'Hello World' if I try to reach it.
+// Exercise 2:
+// Using the same server from Exercise 1 change the GET route to return the user's list.
+// This route should expect a query string that can come empty or not.
+// This query string should be named "q" and will be used to search users by name.
+// If there is no query string you should return the whole list.
+// Example: http://localhost:8989/?q=test
+var dao = require("./userDAO.js");
+
 app.get('/', function(request, response){
-  response.send("Hello World!");
+  var messageIndex = request.query.q;
+  if (messageIndex && messageIndex.length > 0) {
+    dao.searchUsersByName(messageIndex, function(results){
+      response.send(results);
+    });
+  } else {
+    dao.listAllUsers(function(users){
+      response.send(users);
+    });
+  }
 });
 
 app.listen(8989, function(){
